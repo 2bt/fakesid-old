aa62b86fc923e5775c0fd19dde659a05f404c481
twobit <s8572327@gmail.com>
2018-08-06 10:25:21 +0200

    rename package

--- android-project/src/com/sdl/game/Insidious.java
+++
@@ -1,7 +0,0 @@
-package com.sdl.game;
-
-import org.libsdl.app.SDLActivity;
-
-public class Insidious extends SDLActivity {
-
-}
---
+++ android-project/src/com/sdl/insidious/MainActivity.java
@@ -0,0 +1,7 @@
+package com.sdl.insidious;
+
+import org.libsdl.app.SDLActivity;
+
+public class MainActivity extends SDLActivity {
+
+}
